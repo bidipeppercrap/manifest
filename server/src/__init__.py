fc4f480d15703654e7b3da2ece95d7d483ec0db6
Fransisco Wijaya <bidipeppercrap@gmail.com>
2019-07-06 22:57:35 +0700

    SQLAlchemy integration

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,10 +1,13 @@
 import os
 
 from flask import Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+from ..config import set_config
 
 def create_app():
     app = Flask(__name__)
+    set_config(app)
+    db = SQLAlchemy(app)
 
     @app.route("/")
     def index():
